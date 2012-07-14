af9ad380454bdba2bac546067c1743dc6200884a
Janardan Yri <janardan.yri@me.com>
2012-07-14 14:40:03 -0700

    Add rudimentary db photo display.

--- photos.js
+++ photos.js
@@ -1,6 +1,13 @@
+
+Photos = new Meteor.Collection("photos");
+
 if (Meteor.is_client) {
   Template.hello.greeting = function () {
     return "Welcome to photos.";
+  };
+
+  Template.photofeed.photos = function () {
+    return Photos.find({}); //, {sort: {score: -1, name: 1}});
   };
 
   Template.hello.events = {
@@ -8,6 +15,8 @@ if (Meteor.is_client) {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
+      Session.set("buttonpress","beep")
+
     }
   };
 }
@@ -15,5 +24,12 @@ if (Meteor.is_client) {
 if (Meteor.is_server) {
   Meteor.startup(function () {
     // code to run on server at startup
+    if(Photos.find().count() == 0) {
+      urls = ["https://www.google.com/logos/2012/klimt12-hp.jpg",
+              "http://www.google.com/logos/2012/Frantisek_Krizik-2012-hp.jpg"];
+      for (var i = 0; i < urls.length; i++) {        
+        Photos.insert({url: urls[i]});
+      }
+    }
   });
 }
