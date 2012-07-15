dd2be81d3270dcb750452dfc8e3b5e6a58d407dc
Liz Howard <ann.howard@me.com>
2012-07-14 21:48:24 -0700

    Moved API calls to server.

--- imagePopulate.js
+++ imagePopulate.js
@@ -1,18 +1,15 @@
 //imagePopulate.js
 if (Meteor.is_client) {
 	function getFlickrData(searchTerm) {
-		
-	
-	
 		var myData = {};
 		var flickrParams = {
-		tagmode: "any",
-		format: "json"
-			};
+			tagmode: "any",
+			format: "json"
+		};
 
-			if (typeof searchTerm == "string") {
-				flickrParams.tags = searchTerm
-			};
+		if (typeof searchTerm == "string") {
+			flickrParams.tags = searchTerm
+		};
 
 		var flickrRequest = $.getJSON(
 			"http://api.flickr.com/services/feeds/photos_public.gne?&lang=en-us&format=json&jsoncallback=?",
@@ -25,24 +22,27 @@ if (Meteor.is_client) {
 			//clone into returnobject on success
 			//Check photos
 			if(Photos.find().count() > 20) {
-				Photos.remove({});
+				alert("removing photos");
+				Photos.remove();
 			}
 			if(Photos.find().count() < 20) {
 				$.each(myData.items, function(i,item) {
-						if (Photos.findOne({url:item.media.m}) == null) {
+						if (Photos.findOne({url:item.media.m}) == null && Photos.find().count() < 20) {
 							item.url = item.media.m;
-
 							Photos.insert(item);
+						}
+						else {
+							alert("removing photos")
+							Photos.remove();
 						};
 					});
 				}
 		 	});
-		
 	}
 
 	
 
-	$(document).ready(getFlickrData())
+	//$(document).ready(getFlickrData())
 
 	
 }
