475bf2f499c3fcdca9439e18d6f4b01128483700
Liz Howard <ann.howard@me.com>
2012-07-14 18:41:15 -0700

    Adding Image Populating

---
+++ imagePopulate.js
@@ -0,0 +1,34 @@
+//imagePopulate.js
+if (Meteor.is_client) {
+	function getFlickrData(searchTerm) {
+		var myData = {};
+		var flickrParams = {
+		tagmode: "any",
+		format: "json"
+			};
+
+			if (typeof searchTerm == "string") {
+				flickrParams.tags = searchTerm
+			};
+
+		var flickrRequest = $.getJSON(
+			"http://api.flickr.com/services/feeds/photos_public.gne?&lang=en-us&format=json&jsoncallback=?",
+			flickrParams,
+			function(data){
+				$.extend(true,myData,data);
+			});
+
+		flickrRequest.success(function() { 
+			//clone into returnobject on success
+			$.each(myData.items, function(i,item) {
+				
+					item.url = item.media.m;
+					Photos.insert(item);
+				});
+		 	});
+	}
+	getFlickrData("something");
+}
+
+
+
